43b1438b9f6f085884851e89c1be8178f83bdacf
monalisasilvab <monalisasb91@gmail.com>
2024-10-30 22:15:47 -0300

    projeto recebe dados via terminal contendo as caracteristicas de uma conta de banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        Random random = new Random();
+
+        System.out.println("Por favor, digite seu nome e sobrenome!");
+        String nomeCompleto = sc.nextLine();
+
+        System.out.println("Agora, digite o número da Conta!");
+        System.out.print("Conta: ");
+        int numeroConta = sc.nextInt();
+
+        System.out.println("Para finalizar, digite o número da Agência!");
+        System.out.print("Agencia: ");
+        String numeroAgencia = sc.next();
+
+        float saldo = 1000.0f + random.nextFloat() * (10000.0f - 1000.0f);
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$%.2f já está disponível para saque.%n", nomeCompleto, numeroAgencia, numeroConta, saldo);
+
+        sc.close();
     }
 }
